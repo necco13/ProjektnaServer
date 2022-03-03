5f64e9c7a25455e0ecafa975562e321b9f72e6f3
Nejc Narobe <nejc.narobe@students.vegova.si>
2022-03-03 13:22:42 +0100

    First commit

---
+++ Server/Server.js
@@ -0,0 +1,25 @@
+const SerialPort = require('serialport');
+const ReadLine = require('@serialport/parser-readline');
+const port = new SerialPort('COM3', {baudRate: 115200,autoOpen: true});
+
+const WebSocket = require('ws');
+const server = new WebSocket.Server({port:'8080'});
+
+
+server.on('connection',socket =>
+{
+  socket.on('message', podatki => {
+    if(podatki.toString()=="plin")
+      socket.send(plin);
+  });
+});
+
+let plin="";
+
+const parser = new ReadLine();
+port.pipe(parser);
+
+
+parser.on('data', function (podatki) {
+  plin=podatki;
+});
---
+++ Server/Zazeni.bat
@@ -0,0 +1,3 @@
+@echo off
+node Server.js
+pause
