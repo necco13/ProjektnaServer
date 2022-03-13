47ba7260ac8e034f6cc5329440d8fafccf1b2214
Nejc Narobe <nejc.narobe@students.vegova.si>
2022-03-13 16:20:14 +0100

    igrca kocka

--- Uporabnik/Skripti/Igrca.js
+++ Uporabnik/Skripti/Igrca.js
@@ -6,12 +6,14 @@ renderer.setSize(window.innerWidth*0.69, window.innerHeight*0.69 );
 renderer.domElement.style.left="1000px";
 document.body.appendChild( renderer.domElement );
 
-const geometrija = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometrija = new THREE.BoxGeometry(1,1.8,0.5);
+const material = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
 const kocka = new THREE.Mesh( geometrija, material );
 scena.add( kocka );
 
 kamera.position.z = 5;
+kamera.position.y = -5;
+kamera.rotation.x = 1;
 
 function animate() {
 	requestAnimationFrame( animate );
