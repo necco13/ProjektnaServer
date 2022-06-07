aa6c91aadc70a0a74cb1b38b37e2d12290bbcfb4
Nejc Narobe <nejc.narobe@students.vegova.si>
2022-06-08 00:02:23 +0200

    Mapa

--- Uporabnik/Skripti/Igrca.js
+++ Uporabnik/Skripti/Igrca.js
@@ -1,28 +1,46 @@
 const scena = new THREE.Scene();
-const kamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const kamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1,10000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth*0.69, window.innerHeight*0.69 );
 
-const sonce = new THREE.DirectionalLight( 0xfcff96, 0.6);
-sonce.position.set(100,-300,400);
+const sonce = new THREE.DirectionalLight( 0xfcff96, 1);
+sonce.position.set(1200,150,100);
 scena.add(sonce);
 
-const svetloba = new THREE.AmbientLight( 0xffffff, 0.6);
-scena.add(svetloba);
+const light = new THREE.AmbientLight( 0x404040,8 ); // soft white light
+scena.add( light );
 
 scena.background = new THREE.Color(0x87ceeb);
 
 document.body.appendChild( renderer.domElement );
 
+const material = new THREE.MeshPhongMaterial( { vertexColors: true } );
 
-kamera.position.z = 3;
-kamera.position.y = -5;
-kamera.rotation.x = 1.3;
+kamera.position.z = 100;
+kamera.position.y = 400;
+kamera.position.x = 400;
+kamera.rotation.x = 0;
 
-scena.add(naredia());
-scena.add(naredic(0,0));
+var OBJFile = 'Skripti/Cesta.obj';
+var MTLFile = 'Skripti/Cesta.mtl';
+new THREE.MTLLoader()
+.load(MTLFile, function (materials) {
+    materials.preload();
+    new THREE.OBJLoader()
+        .setMaterials(materials)
+        .load(OBJFile, function (object) {
+            object.position.z = - 905;
+            var texture = new THREE.TextureLoader();
 
+            object.traverse(function (child) {   // aka setTexture
+                if (child instanceof THREE.Mesh) {
+                    child.material.map = texture;
+                }
+            });
+            scena.add(object);
+        });
+});
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scena, kamera );
